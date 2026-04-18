51daa15cbcdaf7d6f3252606b5777e604fe02795
DanPancho <daarcosa@puce.edu.ec>
2026-04-18 00:25:54 -0500

    update code

--- scripts/watch_folder.py
+++ scripts/watch_folder.py
@@ -41,8 +41,8 @@ class FileArrivalHandler(FileSystemEventHandler):
             print("Spark ejecutado correctamente.")
             print(result.stdout)
 
-            #os.remove(file_path)
-            #print(f"Archivo eliminado: {file_path}")
+            os.remove(file_path)
+            print(f"Archivo eliminado: {file_path}")
         except subprocess.CalledProcessError as e:
             print("Error ejecutando Spark:")
             print(e.stdout)
@@ -51,31 +51,21 @@ class FileArrivalHandler(FileSystemEventHandler):
 def main():
     WATCH_DIR.mkdir(parents=True, exist_ok=True)
 
-    #event_handler = FileArrivalHandler()
-
-    print("Ejecutando Spark...")
-    result = subprocess.run(
-        SPARK_COMMAND,
-        check=True,
-        text=True,
-        capture_output=True
-    )
-    print("Spark ejecutado correctamente.")
-    print(result.stdout)
+    event_handler = FileArrivalHandler()
     
-    #observer = Observer()
-    #observer.schedule(event_handler, str(WATCH_DIR), recursive=False)
-    #observer.start()
+    observer = Observer()
+    observer.schedule(event_handler, str(WATCH_DIR), recursive=False)
+    observer.start()
 
-    #print(f"Monitoreando carpeta: {WATCH_DIR.resolve()}")
+    print(f"Monitoreando carpeta: {WATCH_DIR.resolve()}")
 
-    #try:
-    #    while True:
-    #        time.sleep(1)
-    #except KeyboardInterrupt:
-    #    observer.stop()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
 
-    #observer.join()
+    observer.join()
 
 if __name__ == "__main__":
     main()
